68370bcd23eea87634d5856927710d190624ac29
andrefreddi <andre.freddi01@gmail.com>
2019-08-27 18:04:13 -0300

    Adicionado mais posts, e linkado os posts com a url

--- services/postsServices.js
+++ services/postsServices.js
@@ -2,6 +2,7 @@
 var getPosts = function(){
     var posts = [
         {
+            id: 1,
             title: 'Post 1',
             image: 'post1.jpg',
             description: 'Meu primeiro post',
@@ -9,10 +10,19 @@ var getPosts = function(){
         },
 
         {
+            id: 2,
             title: 'Post 2',
             image: 'post2.jpg',
             description: 'Meu segundo post',
             body: 'Meu segundo esta voltado aos bot'
+        },
+
+        {
+            id: 3,
+            title: 'Sistema Altomatico de posts :3c',
+            image: '1759.png',
+            description: 'Novo sistema do blog',
+            body: 'Este eh meu segundo post, assim sai meu flint ou o devigle'
         },
 
     ];
