071870aa9050dff76360d9d5ac7d2a24da80706f
andrefreddi <andre.freddi01@gmail.com>
2019-10-29 21:13:55 -0300

    Adcionado o LOGIN ao site

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,8 +12,18 @@ var adminProjectsRouter = require('./routes/admin/project');
 var adminTestimonialsRouter = require('./routes/admin/testimonials');
 var adminNoticiasRouter = require('./routes/admin/noticias');
 var testimonialsRouter = require('./routes/testimonials');
+var authRouter = require('./routes/auth');
+var verifyAuth = require('./middlewares/authMiddleware');
 
 var app = express();
+
+app.set('trust proxy', 1);
+app.use(session({
+  secret: 'mySecret123',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,10 +36,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/admin/posts', adminPostsRouter);
-app.use('/admin/project', adminProjectsRouter);
-app.use('/admin/testimonials', adminTestimonialsRouter);
-app.use('/admin/noticias', adminNoticiasRouter);
+app.use('/auth', authRouter);
+app.use('/admin/posts', [verifyAuth], adminPostsRouter);
+app.use('/admin/project', [verifyAuth], adminProjectsRouter);
+app.use('/admin/testimonials', [verifyAuth], adminTestimonialsRouter);
+app.use('/admin/noticias', [verifyAuth], adminNoticiasRouter);
 app.use('/users', usersRouter);
 app.use('/testimonials', testimonialsRouter);
 
